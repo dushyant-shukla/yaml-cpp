823b72b042f139560f82c1f1808b5348edc2d3cc
Dushyant Shukla <59141668+dushyant-shukla@users.noreply.github.com>
2022-02-27 13:24:24 -0800

    Added premake5.lua script

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
+
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
